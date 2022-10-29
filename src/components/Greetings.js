4e6b062c09e8dbe9d7d352b66730a6ff3339491b
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-29 15:32:24 +0530

    testing the  connected components(Integration Testing)

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Fragment } from "react/cjs/react.production.min"
+import OutPut from "./OutPut";
 
 const Greetings = () => {
     const [changeText, setChangeText] = useState(false);
@@ -10,8 +11,8 @@ const Greetings = () => {
     return (
         <Fragment>
             <h1>Hello World!</h1>
-            {!changeText && <p>It's good to see you</p>}
-            {changeText && <p>It's changed</p>}
+            {!changeText && <OutPut>It's good to see you</OutPut>}
+            {changeText && <OutPut>It's changed</OutPut>}
             <button onClick={changeTextHandler}>Change Text!</button>
         </Fragment>)
 };
