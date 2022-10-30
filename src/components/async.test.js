ba9fcd2818e19eb1be9522402230e81c76bfa46a
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-30 22:25:09 +0530

    mockig functions

--- src/components/async.test.js
+++ src/components/async.test.js
@@ -3,6 +3,13 @@ import Async from "./Async"
 
 describe('Async component', () => {
     test('renders if the request succeeds', async () => {
+        //here we're not sending actual requests instead mock will be sent
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async() => [{
+                id: 'p1', title: 'First post'
+            }]
+        })
         render(<Async />);
         const outputEl = await screen.findAllByRole('listitem');
         expect(outputEl).not.toHaveLength(0);
